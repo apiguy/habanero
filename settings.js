089a4b4ebeca80a343c997d8abecc5473da5d4ae
Freedom Dumlao <Freedom.Dumlao@logmein.com>
2016-03-25 11:52:37 -0400

    making port settable via environment variable

--- settings.js
+++ settings.js
@@ -22,7 +22,7 @@ console.log(__dirname);
 
 module.exports = {
     // the tcp port that the Node-RED web server is listening on
-    uiPort: 1880,
+    uiPort: process.env.PORT || 1880,
 
     // By default, the Node-RED UI accepts connections on all IPv4 interfaces.
     // The following property can be used to listen on a specific interface. For
